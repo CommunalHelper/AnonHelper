45af6f46ffb1af4560089ca02ded416cf5a84cb8
psyGamer <psygamer21@gmail.com>
2022-09-13 12:57:20 +0200

    Added Cloud Refill Lönn plugin

---
+++ Loenn/entities/cloud_refill.lua
@@ -0,0 +1,21 @@
+local utils = require("utils")
+
+local cloudRefill = {}
+
+cloudRefill.name = "Anonhelper/CloudRefill"
+cloudRefill.depth = -100
+cloudRefill.placements = {
+    {
+        name = "cloud_refill",
+        data = {
+            oneUse = false,
+        }
+    }
+}
+cloudRefill.texture = "objects/AnonHelper/cloudRefill/idle00"
+
+function cloudRefill.selection(room, entity)
+	return utils.rectangle(entity.x - 5, entity.y - 4, 11, 7)
+end
+
+return cloudRefill
