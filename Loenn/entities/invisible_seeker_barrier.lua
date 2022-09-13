a7d174688d132e06b314e57d09983e641e0beb45
psyGamer <psygamer21@gmail.com>
2022-09-13 13:03:07 +0200

    Added Invisible Seeker Barrier Lönn plugin

---
+++ Loenn/entities/invisible_seeker_barrier.lua
@@ -0,0 +1,14 @@
+local invisibleSeekerBarrier = {}
+
+invisibleSeekerBarrier.name = "Anonhelper/InvisibleSeekerBarrier"
+invisibleSeekerBarrier.fillColor = {0.25, 0.25, 0.25, 0.8}
+invisibleSeekerBarrier.borderColor = {0.0, 0.0, 0.0, 0.0}
+invisibleSeekerBarrier.placements = {
+    name = "invisible_seeker_barrier",
+    data = {
+        width = 8,
+        height = 8
+    }
+}
+
+return invisibleSeekerBarrier
