fc38fbc09eb025b2337bb631c3a066efd46c1b61
Vivian <71410564+Viv-0@users.noreply.github.com>
2023-06-11 04:39:16 -0400

    Create booster_refill.lua

---
+++ Loenn/entities/booster_refill.lua
@@ -0,0 +1,32 @@
+local utils = require("utils")
+
+local boostRefill = {}
+
+boostRefill.name = "Anonhelper/BoosterRefill"
+boostRefill.depth = -100
+boostRefill.placements = {
+    {
+        name = "boost_refill",
+        data = {
+            oneUse = false,
+            boostOnEnd = false,
+        }
+    }
+}
+function boostRefill.texture(room, entity)
+    local red = entity.boostOnEnd
+
+    if red then
+        return "objects/AnonHelper/boosterRefillOnDash/"
+
+    else
+        return "objects/AnonHelper/boosterRefill/"
+    end
+end
+
+function boostRefill.selection(room, entity)
+	local x, y = entity.x or 0, entity.y or 0
+    return utils.rectangle(x - 5, y - 4, 10, 9)
+end
+
+return boostRefill
